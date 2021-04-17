36e003ba61168c5a3b8b53f9dfe2fd613f0286cb
Oksana-Mazur58 <72046543+Oksana-Mazur58@users.noreply.github.com>
2021-04-17 19:54:52 +0300

    Додала розмітку для одної країни

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './styles.css';
+import countryCardTpl from './templates/country_card.hbs'
+
+fetch('https://restcountries.eu/rest/v2/name/Ukraine')
+    .then(response => {
+    return response.json()
+})
+    .then(country => {
+        console.log(country);
+        const markup = countryCardTpl(country)
+        console.log (markup)
+    })
+    .catch(error => {
+        console.log(error);
+})
