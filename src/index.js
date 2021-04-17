0644be906f290e61f853215bbec4ba7456970b3d
Oksana-Mazur58 <72046543+Oksana-Mazur58@users.noreply.github.com>
2021-04-17 21:33:35 +0300

    шукає одну країну

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import './styles.css';
 import countryCardTpl from './templates/country_card.hbs'
+import debounce from 'lodash.debounce';
 
-fetch('https://restcountries.eu/rest/v2/name/Ukraine')
-    .then(response => {
-    return response.json()
-})
-    .then(country => {
-        console.log(country);
-        const markup = countryCardTpl(country)
-        console.log (markup)
-    })
-    .catch(error => {
-        console.log(error);
-})
+
+const refs = {
+    countryContainer: document.querySelector('.js-country-container'),
+    input: document.querySelector('.input')
+}
+
+refs.input.addEventListener('input', debounce(onFindCountry,2000))
+
+function onFindCountry(e) {
+
+    const countryName = e.target.value
+
+    fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
+        .then(response => {
+            return response.json()
+        })
+        .then(country => {
+            console.log(country);
+            const markup = countryCardTpl(country[0])
+            console.log(markup)
+            refs.countryContainer.innerHTML = markup;
+
+        })
+        .catch(error => {
+            console.log(error);
+        })  
+}
+
