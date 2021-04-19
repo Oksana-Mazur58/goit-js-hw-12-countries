057127bd8b87137a238f8c51eaa1a4eab88df6d3
Oksana-Mazur58 <72046543+Oksana-Mazur58@users.noreply.github.com>
2021-04-19 11:32:11 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,10 @@
 export default (searchQuery) => {
     const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`
    return fetch(url)
-        .then(response => {
+       .then(response => {
+            if (!response.ok) {
+                throw error;
+            }
             return response.json()
         })
 }
